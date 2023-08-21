9186c9b78893360fe2ee2ba113e2a60b6b6c43a5
Nico04 <34476051+Nico04@users.noreply.github.com>
2023-08-21 13:33:49 +0200

    rename var in test

--- test/value_stream_test.dart
+++ test/value_stream_test.dart
@@ -116,130 +116,130 @@ void main() {
 
   group('EventStream', () {
     test('takes initialValue', () {
-      final vs = EventStream(42);
-      expect(vs.valueOrNull, 42);
-      vs.close();
+      final es = EventStream(42);
+      expect(es.valueOrNull, 42);
+      es.close();
     });
 
     test('updates value on add()', () {
-      final vs = EventStream(42);
-      expect(vs.valueOrNull, 42);
+      final es = EventStream(42);
+      expect(es.valueOrNull, 42);
 
-      vs.add(43);
-      expect(vs.valueOrNull, 43);
+      es.add(43);
+      expect(es.valueOrNull, 43);
 
-      vs.close();
+      es.close();
     });
 
     test('only latest value', () {
-      final vs = EventStream(1);
+      final es = EventStream(1);
 
-      vs.add(2);
-      vs.add(3);
-      vs.add(4);
-      expect(vs.valueOrNull, 4);
+      es.add(2);
+      es.add(3);
+      es.add(4);
+      expect(es.valueOrNull, 4);
 
-      vs.close();
+      es.close();
     });
 
     test('works with nullables', () {
-      final vs = EventStream<int?>(null);
-      expect(vs.valueOrNull, isNull);
+      final es = EventStream<int?>(null);
+      expect(es.valueOrNull, isNull);
 
-      vs.add(45);
-      expect(vs.valueOrNull, 45);
+      es.add(45);
+      expect(es.valueOrNull, 45);
 
-      vs.add(null);
-      expect(vs.valueOrNull, isNull);
+      es.add(null);
+      expect(es.valueOrNull, isNull);
 
-      vs.add(48);
-      expect(vs.valueOrNull, 48);
+      es.add(48);
+      expect(es.valueOrNull, 48);
 
-      vs.close();
+      es.close();
     });
 
     test('isClosed set after close()', () {
-      final vs = EventStream(42);
-      expect(vs.isClosed, isFalse);
+      final es = EventStream(42);
+      expect(es.isClosed, isFalse);
 
-      vs.close();
-      expect(vs.isClosed, isTrue);
+      es.close();
+      expect(es.isClosed, isTrue);
     });
 
     test('listen() push updates', () async {
-      final vs = EventStream(0);
-      expect(vs.valueOrNull, 0);
+      final es = EventStream(0);
+      expect(es.valueOrNull, 0);
 
       final values = <int>[];
-      final ss = vs.listen(values.add);
+      final ss = es.listen(values.add);
 
-      vs.add(1);
-      vs.add(2);
-      vs.add(3);
+      es.add(1);
+      es.add(2);
+      es.add(3);
 
       await Future.delayed(const Duration(milliseconds: 1));
       expect(values, [1, 2, 3]);
-      expect(vs.valueOrNull, 3);
+      expect(es.valueOrNull, 3);
 
       ss.cancel();
-      vs.close();
+      es.close();
     });
 
     test('add() arguments', () async {
-      final vs = EventStream<int?>(0);
-      expect(vs.valueOrNull, 0);
+      final es = EventStream<int?>(0);
+      expect(es.valueOrNull, 0);
 
       final values = <int?>[];
-      final ss = vs.listen(values.add);
+      final ss = es.listen(values.add);
 
-      vs.add(1);
-      vs.add(1);
-      vs.add(1, skipSame: true);
-      vs.add(1);
+      es.add(1);
+      es.add(1);
+      es.add(1, skipSame: true);
+      es.add(1);
 
-      vs.add(null);
-      vs.add(null, skipNull: true);
-      vs.add(null);
+      es.add(null);
+      es.add(null, skipNull: true);
+      es.add(null);
 
-      vs.add(2, skipIfClosed: true);
-      vs.add(3, skipIfClosed: true, skipNull: true, skipSame: true);
+      es.add(2, skipIfClosed: true);
+      es.add(3, skipIfClosed: true, skipNull: true, skipSame: true);
 
-      vs.close();
-      expect(vs.add(4, skipIfClosed: true), isFalse);
-      expect(() => vs.add(4), throwsA(isA<Error>()));
+      es.close();
+      expect(es.add(4, skipIfClosed: true), isFalse);
+      expect(() => es.add(4), throwsA(isA<Error>()));
 
       await Future.delayed(const Duration(milliseconds: 1));
       expect(values, [1, 1, 1, null, null, 2, 3]);
-      expect(vs.valueOrNull, 3);
+      expect(es.valueOrNull, 3);
 
       ss.cancel();
-      vs.close();
+      es.close();
     });
 
     test('updates error & value on addError() & add()', () {
-      final vs = EventStream(42);
-      expect(vs.valueOrNull, 42);
+      final es = EventStream(42);
+      expect(es.valueOrNull, 42);
 
-      vs.addError(Error());
-      expect(vs.valueOrNull, isNull);
-      expect(vs.error, isA<Error>());
+      es.addError(Error());
+      expect(es.valueOrNull, isNull);
+      expect(es.error, isA<Error>());
 
-      vs.add(100);
-      expect(vs.valueOrNull, 100);
-      expect(vs.error, isNull);
+      es.add(100);
+      expect(es.valueOrNull, 100);
+      expect(es.error, isNull);
 
-      vs.close();
+      es.close();
     });
 
     test('first getter', () async {
-      final vs = EventStream();
-      expect(vs.valueOrNull, isNull);
-      final firstFuture = vs.first;
-      vs.add(42);
+      final es = EventStream();
+      expect(es.valueOrNull, isNull);
+      final firstFuture = es.first;
+      es.add(42);
       final first = await firstFuture;
       expect(first, 42);
 
-      vs.close();
+      es.close();
     });
   });
 }
